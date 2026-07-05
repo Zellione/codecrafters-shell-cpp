eaf32f62d219c07a7bb11e3fb77815b438d6c456
Zellione <nagels.sebastian@gmail.com>
2026-07-05 23:25:08 +0200

    codecrafters submit [skip ci]

--- src/commands/builtin/jobs.cpp
+++ src/commands/builtin/jobs.cpp
@@ -13,12 +13,21 @@ int JobsCommand::Process(const std::vector<Token> &tokens) const
     std::map<unsigned int, BackgroundJob> jobs = m_registry.GetAll();
     for (auto it = jobs.begin(); it != jobs.end(); it++)
     {
+        char job_marker = ' ';
+        if (std::next(it) == jobs.end())
+        {
+            job_marker = '+';
+        }
+        if (std::next(it) == std::prev(jobs.end()))
+        {
+            job_marker = '-';
+        }
+
         // Status column (Running) is padded to 24 characters (7 characters + 17
         // characters space)
         m_output->Put(tokens,
                       std::format("[{}]{}  Running                 {}\n",
-                                  it->first,
-                                  std::next(it) == jobs.end() ? "+" : "",
+                                  it->first, job_marker,
                                   it->second.commandline),
                       OutputTarget::STDOUT);
     }
