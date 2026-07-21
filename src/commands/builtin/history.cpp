5c24192193e4723e5fc8de6b29e7eb59e917a851
Zellione <nagels.sebastian@gmail.com>
2026-07-21 02:43:57 +0200

    codecrafters submit [skip ci]

--- src/commands/builtin/history.cpp
+++ src/commands/builtin/history.cpp
@@ -11,8 +11,15 @@ HistoryCommand::HistoryCommand(HistoryRegistry *registry)
 
 int HistoryCommand::Process(const Command &comm) const
 {
+    size_t limit = 0;
+    if (comm.Args.size() > 1)
+    {
+        limit = std::stoull(comm.Args[1]);
+    }
+
     const auto &entries = m_registry->Get();
-    for (size_t i = 0; i < entries.size(); i++)
+    for (size_t i = limit > 0 ? entries.size() - limit : 0; i < entries.size();
+         i++)
     {
         std::cout << std::format("    {}  {}\n", i + 1, entries[i]);
     }
