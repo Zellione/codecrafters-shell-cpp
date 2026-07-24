6dc70b834eb2ac5bda04ef24351471b68839bf38
Zellione <nagels.sebastian@gmail.com>
2026-07-25 01:33:22 +0200

    codecrafters submit [skip ci]

--- src/commands/builtin/declare.cpp
+++ src/commands/builtin/declare.cpp
@@ -4,4 +4,18 @@ using Ast::Command;
 
 DeclareCommand::DeclareCommand() : BuiltinCommand("declare") {}
 
-int DeclareCommand::Process(const Command &comm) const { return 0; }
+int DeclareCommand::Process(const Command &comm) const
+{
+    std::string variable_name;
+    for (size_t i = 1; i < comm.Args.size(); i++)
+    {
+        if (comm.Args[i] == "-p" && (i + 1) < comm.Args.size())
+        {
+            variable_name = comm.Args[++i];
+        }
+    }
+
+    std::cout << std::format("declare: {}: not found\n", variable_name);
+
+    return 0;
+}
